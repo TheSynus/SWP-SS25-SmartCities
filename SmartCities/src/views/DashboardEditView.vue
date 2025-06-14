bfa8cec2003416a4cb37238981d8b812442c55db
Jannis <stud106062@fh-wedel.de>
2025-06-14 19:29:32 +0200

    dashboard edit start

--- SmartCities/src/views/DashboardEditView.vue
+++ SmartCities/src/views/DashboardEditView.vue
@@ -1,6 +1,45 @@
 <script setup lang="ts">
+import DefaultCard from '../components/DefaultCard.vue'
+import AddCardButton from '../components/dashboardEdit/AddCardButton.vue'
+import PhoneMockup from '../components/dashboardEdit/PhoneMockup.vue'
 </script>
 
 <template>
-  <h1>DASHBOARD EDIT</h1>
+  <div class="grid grid-cols-2 overflow-hidden h-[calc(100vh-64px)]">
+    <!-- Left Column -->
+    <!-- <div class="self-start">PLATZHALTER</div> -->
+
+    <!-- Scrollable Middle Column -->
+    <div
+      class="flex justify-center overflow-y-scroll h-full custom-scrollbar"
+    >
+      <ul>
+        <AddCardButton />
+        <li class="py-4"><DefaultCard /></li>
+        <AddCardButton />
+        <li class="py-4"><DefaultCard /></li>
+        <AddCardButton />
+        <li class="py-4"><DefaultCard /></li>
+        <AddCardButton />
+        <li class="py-4"><DefaultCard /></li>
+        <AddCardButton />
+        <li class="py-4"><DefaultCard /></li>
+        <AddCardButton />
+        <li class="py-4"><DefaultCard /></li>
+        <AddCardButton />
+      </ul>
+    </div>
+
+    <!-- Right Column -->
+    <div class="self-start">
+      <PhoneMockup />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+/* Hide scrollbar arrows */
+::-webkit-scrollbar-button {
+  display: none;
+}
+</style>
