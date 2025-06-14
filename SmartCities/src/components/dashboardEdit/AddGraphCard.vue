e20013a2b0e6366ae34388873cf009ccc0aff392
Jannis <stud106062@fh-wedel.de>
2025-06-15 01:13:16 +0200

    mockup grafkacheln

--- SmartCities/src/components/dashboardEdit/AddGraphCard.vue
+++ SmartCities/src/components/dashboardEdit/AddGraphCard.vue
@@ -1,33 +1,23 @@
 <script setup lang="ts">
-import DefaultCard from '../DefaultCard.vue'
+import CardGraphLine from '../CardGraphLine.vue'
+import CardGraphColumn from '../CardGraphColumn.vue'
+import CardGraphBar from '../CardGraphBar.vue'
+import CardGraphPie from '../CardGraphPie.vue'
 </script>
 
 <template>
-  <div class="grid grid-cols-3">
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
-    <DefaultCard />
+  <div class="grid grid-cols-2 place-items-center gap-y-4">
+    <div class="h-80">
+      <CardGraphLine />
+    </div>
+    <div class="h-80">
+      <CardGraphColumn />
+    </div>
+    <div class="h-80">
+      <CardGraphBar />
+    </div>
+    <div class="h-80">
+      <CardGraphPie />
+    </div>
   </div>
 </template>
