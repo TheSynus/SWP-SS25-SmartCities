fddfb6cc4e2b57121420aa5afa548ddd426ea209
simon <simon.keitel@hamburg.de>
2025-06-17 19:51:59 +0200

    Setup-Routen repariert
    Setup-Routen repariert: dazu Objekt der Werte in der index.js angelget;
    setupRouter.js angepast, um Parameter aus index.js entgegenzunehmen;
    durch Objekt sind Änderungen dateiübergreifend (sollten sie jedenfalls)

--- SmartCities_Backend/api/src/index.js
+++ SmartCities_Backend/api/src/index.js
@@ -20,6 +20,7 @@ app.use(cors({
   origin: 'http://localhost:8000',
 }));
 
+// Kann das hier raus?
 require('dotenv').config();
 
 app.use('/test/', testRouter)
@@ -27,13 +28,20 @@ app.use('/images/', imagesRouter)
 app.use('/events/', eventsRouter)
 
 // Variablen:
-let plz = null; 
-let city = null;
-let regionalKey = null;
+//let plz = null; 
+//let city = null;
+//let regionalKey = null;
 
-app.use('/test/', testRouter)
-app.use('/setup/', setupRouter)
-app.use('/nina/', ninaRouter)
+// Speichern der ursprünglichen Variablen in Objekt, um durchreichen, verändern & zurückgeben für Router zu ermöglichen
+const configValues = {
+  plz: null,
+  city: null,
+  regionalKey: null,
+};
+
+app.use('/test/', testRouter);
+app.use('/setup/', setupRouter(configValues, { getCityToPLZ, getRegionalKey, writeValueToJSON }));
+app.use('/nina/', ninaRouter);
 
 app.listen(port, '0.0.0.0', () => {
   console.log("----------- Serverstart -----------")
@@ -42,11 +50,11 @@ app.listen(port, '0.0.0.0', () => {
   //Initialisieren der Serverconfigwerte aus der JSON
   console.log("Initialisieren der Servervariablen:")
   const configJsonContent = getJSONContent("./config.json");
-  plz = configJsonContent.plz;
-  console.log("Postal code from Config:", plz);
-  city = configJsonContent.cityName;
-  console.log("City name from Config:", city);
-  regionalKey = configJsonContent.regionalKey;
-  console.log("Regional key from Config:", regionalKey);
+  configValues.plz = configJsonContent.plz;
+  console.log("Postal code from Config:", configValues.plz);
+  configValues.city = configJsonContent.cityName;
+  console.log("City name from Config:", configValues.city);
+  configValues.regionalKey = configJsonContent.regionalKey;
+  console.log("Regional key from Config:", configValues.regionalKey);
   console.log("-----------------------------------")
 });
--- SmartCities_Backend/api/src/router/setupRouter.js
+++ SmartCities_Backend/api/src/router/setupRouter.js
@@ -1,69 +1,80 @@
 const express = require('express');
-const router = express.Router()
 
-// Route zum initialen Annehmen der Postleitzahl und automatischen Ermitteln der Stadt
-// Request Body: JSON Objekt mit "plz"-Field, also z.B.: { "plz" : "12345" }
-router.post('/plz', async (req, res) => {
-  
-  if (plz !== -1) {
-    console.error('USER-ERROR: tried to set postal code, postal code was already set.');
-    return res.status(400).send('The postal code was already set');
-  }
+// Erstellt eine factoryFunction, die bei Aufruf einen Router erstellt, der angegebene Argumente annimmt
+// Dadurch lassen sich Variablen und Funktionen übergeben
+module.exports = (configValues, utils) => {
+  const router = express.Router()
+  const { getCityToPLZ, getRegionalKey, writeValueToJSON } = utils;
 
-  const { plz: newPlz } = req.body;
-  // Fehler, wenn keine Postleitzahl mitgeschickt wurde
-  if (!newPlz) {
-    console.error('USER-ERROR: tried to set postal code, no postal code provided.');
-    return res.status(400).send('A postal code is required');
-  }
+  // Route zum initialen Annehmen der Postleitzahl und automatischen Ermitteln der Stadt
+  // Request Body: JSON Objekt mit "plz"-Field, also z.B.: { "plz" : "12345" }
+  router.post('/plz', async (req, res) => {
+    
+    if (configValues.plz !== -1) {
+      console.error('USER-ERROR: tried to set postal code, postal code was already set.');
+      return res.status(400).send('The postal code was already set');
+    }
 
-  // Überprüfen, ob die geschickte Postleitzahl dem richtigen Format entspricht
-  const isValidPLZ = /^\d{5}$/.test(newPlz);
-  if (!isValidPLZ) {
-    console.error('USER-ERROR: tried to set postal code, provided postal code not in valid format.');
-    return res.status(400).send('The postal code is not in a valid format. It must be exactly 5 digits long and only contain numbers.');
-  }
+    const { plz: newPlz } = req.body;
+    // Fehler, wenn keine Postleitzahl mitgeschickt wurde
+    if (!newPlz) {
+      console.error('USER-ERROR: tried to set postal code, no postal code provided.');
+      return res.status(400).send('A postal code is required');
+    }
 
-  // Wenn keine Fehler auftreten, wird die neue Postleitzahl gesetzt
-  plz = newPlz;
-  writeValueToJSON("./config.json", "plz", plz);
+    // Überprüfen, ob die geschickte Postleitzahl dem richtigen Format entspricht
+    const isValidPLZ = /^\d{5}$/.test(newPlz);
+    if (!isValidPLZ) {
+      console.error('USER-ERROR: tried to set postal code, provided postal code not in valid format.');
+      return res.status(400).send('The postal code is not in a valid format. It must be exactly 5 digits long and only contain numbers.');
+    }
 
-  res.send('The postal code set successfully');
-  console.log('Postal code was set to: ' + plz);
+    // Wenn keine Fehler auftreten, wird die neue Postleitzahl gesetzt
+    configValues.plz = newPlz;
+    writeValueToJSON("./config.json", "plz", configValues.plz);
 
-  city = await getCityToPLZ(plz);
-  writeValueToJSON("./config.json", "cityName", city);
+    res.send('The postal code set successfully');
+    console.log('Postal code was set to: ' + configValues.plz);
 
-  //regionalKey = getRegionalKey(city);
-  //writeValueToJSON("./config.json", "regionalKey", regionalKey);
-});
+    configValues.city = await getCityToPLZ(configValues.plz);
+    writeValueToJSON("./config.json", "cityName", configValues.city);
 
-// Route zum initialen Annehmen des Regionalschlüssels
-// Request Body: JSON Objekt mit "regionalKey"-Field, also z.B.: { "regionalKey" : "012345678912" }
-router.post('/regionalKey', async (req, res) => {
-  // Fehler, wenn der Regionalschlüssel bereits gesetzt wurde
-  if (regionalKey !== -1) {
-    console.error('USER-ERROR: tried to set regional key, regional key was already set.');
-    return res.status(400).send('The regional key was already set');
-  }
+    //regionalKey = getRegionalKey(city);
+    //writeValueToJSON("./config.json", "regionalKey", regionalKey);
+  });
 
-  const { regionalKey : newRegionalKey } = req.body;
-  // Fehler, wenn kein Regionalschlüssel mitgeschickt wurde
-  if (!newRegionalKey) {
-    console.error('USER-ERROR: tried to set regional key, no regional key provided');
-    return res.status(400).send('A regional key is required');
-  }
+  // Route zum initialen Annehmen des Regionalschlüssels
+  // Request Body: JSON Objekt mit "regionalKey"-Field, also z.B.: { "regionalKey" : "012345678912" }
+  router.post('/regionalKey', async (req, res) => {
+    // Fehler, wenn der Regionalschlüssel bereits gesetzt wurde
+    if (configValues.regionalKey !== -1) {
+      console.error('USER-ERROR: tried to set regional key, regional key was already set.');
+      return res.status(400).send('The regional key was already set');
+    }
 
-  // Überprüfen, ob Regionalschlüssel dem richtigen Format entspricht - 12 Zahlen
-  const isValidKey = /^\d{12}$/.test(newRegionalKey);
-  if (!isValidKey) {
-    console.error('USER-ERROR: tried to set regional key, provided regional key not in valid format');
-    return res.status(400).send('The regional key is not in a valid format. It must be exactly 12 digits long and only contain numbers.');
-  }
+    const { regionalKey : newRegionalKey } = req.body;
+    // Fehler, wenn kein Regionalschlüssel mitgeschickt wurde
+    if (!newRegionalKey) {
+      console.error('USER-ERROR: tried to set regional key, no regional key provided');
+      return res.status(400).send('A regional key is required');
+    }
 
-  // Wenn keine Fehler auftreten, wird der Regionalschlüssel gesetzt
-  regionalKey = newRegionalKey;
-  writeValueToJSON("./config.json", "regionalKey", regionalKey);
-});
+    // Überprüfen, ob Regionalschlüssel dem richtigen Format entspricht - 12 Zahlen
+    const isValidKey = /^\d{12}$/.test(newRegionalKey);
+    if (!isValidKey) {
+      console.error('USER-ERROR: tried to set regional key, provided regional key not in valid format');
+      return res.status(400).send('The regional key is not in a valid format. It must be exactly 12 digits long and only contain numbers.');
+    }
 
-module.exports = router;
+    // Wenn keine Fehler auftreten, wird der Regionalschlüssel gesetzt
+    configValues.regionalKey = newRegionalKey;
+    writeValueToJSON("./config.json", "regionalKey", configValues.regionalKey);
+
+    res.status(200).send('The regional key set successfully');
+    console.log('Regional key was set to: ' + configValues.regionalKey);
+  });
+
+  //module.exports = router;
+  return router;
+
+}
