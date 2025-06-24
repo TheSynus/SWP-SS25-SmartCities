b09c23578efb3106a6fbcece0b8691ae7f1fab5e
Hendrik <hendrikbotner@gmail.com>
2025-06-24 15:24:00 +0200

    Cors Route angepasst

--- SmartCities_Backend/api/src/index.js
+++ SmartCities_Backend/api/src/index.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:8000',
+  origin: 'http://localhost:5173',
 }));
 
 // Kann das hier raus?
